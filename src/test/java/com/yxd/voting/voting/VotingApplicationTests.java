2c699e46e94c246635b84bb2ec91b1ac746c847a
yxd1 <yxd970530@163.com>
2021-04-11 20:10:25 +0800

    init repository

---
+++ src/test/java/com/yxd/voting/voting/VotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxd.voting.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
